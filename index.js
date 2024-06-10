5836927e28b10852859064110488c6e79406e891
tdnguyen98 <theo-nguy@hotmail.com>
2024-06-10 11:15:08 +0200

    tried to fixe issue 01

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let sound = document.createElement('audio')
 sound.id = 'audio'
 sound.controls = ''
-sound.src = '/assets/backsound.mp3'
+sound.src = 'assets/backsound.mp3'
 sound.type = 'audio/mp3'
+sound.muted = true;
 document.body.appendChild(sound)
-
-function playAudio() {
-  document.getElementById('audio').play();
-}
-
-setTimeout("playAudio()", 2700);
 
 let mutedButton = document.getElementById('mutedBtn')
 
 mutedButton.onclick = function (e) {
-  sound.muted = true
+  if (sound.muted) {
+    sound.muted = false;
+    sound.play();
+  } else {
+    sound.muted = true;
+    sound.pause();
+  }
 };
